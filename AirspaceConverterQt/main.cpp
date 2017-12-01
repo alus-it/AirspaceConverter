70d4d41017e345107357a43f3085e084045828c1
Valerio <efa@iol.it>
2017-12-01 02:15:47 +0100

    added: setlocale(LC_ALL, "C"); in AirspaceConverterQt/main.cpp so std::stod recognize dot as decimal separator added test optional parameter to debianize.sh Typo in AirspaceConverter.cpp

--- AirspaceConverterQt/main.cpp
+++ AirspaceConverterQt/main.cpp
@@ -15,6 +15,7 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    setlocale(LC_ALL, "C"); // so std::stod recognize dot as decimal separator
     MainWindow w;
     w.show();
     return a.exec();
