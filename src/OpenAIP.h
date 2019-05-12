7a1593c029e715a46a92db545af81d903b4b7e69
alus- <admin@alus.it>
2019-05-12 12:17:41 +0200

    Preparing to parse openAIP waypoint and adapting to new log functions

--- src/OpenAIP.h
+++ src/OpenAIP.h
@@ -29,6 +29,15 @@ public:
 
 private:
 	static bool ParseAltitude(const boost::property_tree::ptree& node, Altitude& altitude);
+	static bool ParseGeolocation(const boost::property_tree::ptree& parentNode, double &lat, double &lon, double &alt);
+	static bool ParseContent(const boost::property_tree::ptree& parentNode, const std::string& tagName, std::string& outputString);
+	static bool ParseAttribute(const boost::property_tree::ptree& node, const std::string& attributeName, std::string& outputString);
+	static bool ParseValue(const boost::property_tree::ptree& parentNode, const std::string& tagName, double &value);
+	static bool ParseMeasurement(const boost::property_tree::ptree& parentNode, const std::string&  tagName, char expectedUnit, double &value);
+
+	bool ParseAirports(const boost::property_tree::ptree& airportsNode);
+	bool ParseNavAids(const boost::property_tree::ptree& navAidsNode);
+	bool ParseHotSpots(const boost::property_tree::ptree& hotSpotsNode);
 
 	std::multimap<int,Airspace>& airspaces;
 	std::multimap<int,Waypoint*>& waypoints;
