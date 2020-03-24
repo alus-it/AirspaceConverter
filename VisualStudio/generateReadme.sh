951f4891b13d25789604a334567e66f6931726ad
alus- <admin@alus.it>
2020-03-24 13:06:09 +0100

    Readme.rtf can be generated from original markdown readme

---
+++ VisualStudio/generateReadme.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#============================================================================
+# AirspaceConverter
+# Since       : 24/3/2020
+# Author      : Alberto Realis-Luc <alberto.realisluc@gmail.com>
+# Web         : https://www.alus.it/AirspaceConverter
+# Copyright   : (C) 2016-2020 Alberto Realis-Luc
+# License     : GNU GPL v3
+#
+# This script is part of AirspaceConverter project
+#============================================================================
+
+echo Generating Readme.rtf...
+pandoc -s ../README.md -o Readme.rtf
