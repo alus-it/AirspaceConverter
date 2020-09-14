305dc014284fc22c2ffe10cfa7d6bbc669ca56d4
Valerio Messina <efa@iol.it>
2020-09-15 01:13:26 +0200

    Second push for CSV support, export now is quite complete CUP:Direction,Length,Frequency are written to CSV:Label field In CSV elevation is always feet and unit must be omitted Frequency for VOR and NDB use 2 digits, while for Airports use 3

--- test/tstCsv.sh
+++ test/tstCsv.sh
@@ -20,11 +20,11 @@ if (test "$?" != 0) then
 fi
 echo Done
 cd Release
-#airspaceconverter -w ~/Documents/Flight/AIP/ITA_WPT_30-JAN-2020-2002_V02.cup -o ~/Documents/Flight/AIP/tst.csv
-airspaceconverter -w ~/Documents/Flight/AIP/tst.cup -o ~/Documents/Flight/AIP/tst.csv
+#airspaceconverter -w ~/Documents/Flight/AIP/tst.cup -o ~/Documents/Flight/AIP/tst.csv
+airspaceconverter -w ~/Documents/Flight/AIP/ITA_WPT_10-SEP-2020-2010_V02.cup -o ~/Documents/Flight/AIP/tst.csv
 echo Sorting ...
 sort ~/Documents/Flight/AIP/tst.csv > ~/Documents/Flight/AIP/tst2.csv
 mv ~/Documents/Flight/AIP/tst2.csv ~/Documents/Flight/AIP/tst.csv
 echo Comparing ...
-#diff ~/Documents/Flight/AIP/ITA_WPT_30-JAN-2020-2002_V02.csv ~/Documents/Flight/AIP/tst.csv
-diff ~/Documents/Flight/AIP/ref.csv ~/Documents/Flight/AIP/tst.csv
+#diff --strip-trailing-cr ~/Documents/Flight/AIP/ref.csv ~/Documents/Flight/AIP/tst.csv > ~/Documents/Flight/AIP/diff.txt
+diff --strip-trailing-cr ~/Documents/Flight/AIP/ITA_WPT_10-SEP-2020-2010_V02.csv ~/Documents/Flight/AIP/tst.csv > ~/Documents/Flight/AIP/diff.txt
